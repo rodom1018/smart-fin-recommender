0817c22a3cf63967c7f4a44027148f321650bc13
rodom_macbook <enjoying1018@naver.com>
2025-05-18 11:00:12 +0900

    initial commit

---
+++ src/main/java/com/example/smart_fin_recommender/SmartFinRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_fin_recommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFinRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFinRecommenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smart_fin_recommender/SmartFinRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_fin_recommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
